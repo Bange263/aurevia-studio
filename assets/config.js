1396dea56df82024be61e3f9250c75a5776675dd
Bange263 <if425785@gmail.com>
2026-08-24 23:19:42 +0200

    Send landing CTAs directly to Payhip checkout

--- assets/config.js
+++ assets/config.js
@@ -4,7 +4,7 @@
 */
 window.AUREVIA_CONFIG = {
   supportEmail: "if425785@gmail.com",
-  storeUrl: "https://payhip.com/b/TpkFC",
+  storeUrl: "https://payhip.com/buy?link=TpkFC",
   sellerDisplayName: "Aurevia Studio",
   productPrice: "€29.99",
   productVersion: "1.2.0"
